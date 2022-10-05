d3ad257257f3b7d3aedfc9364f3a1ae8eb8a9127
huytang99 <68282036+huytang99@users.noreply.github.com>
2022-10-05 13:56:56 +0700

    initial commit

--- Core/Src/fsm_automatic.c
+++ Core/Src/fsm_automatic.c
@@ -9,42 +9,42 @@
 
 void fsm_automatic_run() {
 	switch(status) {
-	case INIT:
-		HAL_GPIO_WritePin ( LED_RED_GPIO_Port , LED_RED_Pin , GPIO_PIN_SET );
-		HAL_GPIO_WritePin ( LED_YELLOW_GPIO_Port , LED_YELLOW_Pin , GPIO_PIN_SET );
-		HAL_GPIO_WritePin ( LED_GREEN_GPIO_Port , LED_GREEN_Pin , GPIO_PIN_SET );
-		status = AUTO_RED;
-		setTimer1(5000);
-		break;
-	case AUTO_RED:
-		//TODO
-		HAL_GPIO_WritePin ( LED_RED_GPIO_Port , LED_RED_Pin , GPIO_PIN_RESET );
-		HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, GPIO_PIN_SET);
-		if(timer1_flag == 1) {
-			status = AUTO_GREEN;
-			setTimer1(3000);
-		}
-		break;
-	case AUTO_GREEN:
-		HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_RESET);
-		HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_SET);
-		//TODO
-		if(timer1_flag == 1) {
-			status = AUTO_YELLOW;
-			setTimer1(2000);
-		}
-		break;
-	case AUTO_YELLOW:
-		//TODO
-		HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, GPIO_PIN_RESET);
-		HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_SET);
-		if(timer1_flag == 1) {
+		case INIT:
+			HAL_GPIO_WritePin ( LED_RED_GPIO_Port , LED_RED_Pin , GPIO_PIN_SET );
+			HAL_GPIO_WritePin ( LED_YELLOW_GPIO_Port , LED_YELLOW_Pin , GPIO_PIN_SET );
+			HAL_GPIO_WritePin ( LED_GREEN_GPIO_Port , LED_GREEN_Pin , GPIO_PIN_SET );
 			status = AUTO_RED;
 			setTimer1(5000);
-		}
-		break;
-	default:
-		break;
+			break;
+		case AUTO_RED:
+			//TODO
+			HAL_GPIO_WritePin ( LED_RED_GPIO_Port , LED_RED_Pin , GPIO_PIN_RESET );
+			HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, GPIO_PIN_SET);
+			if(timer1_flag == 1) {
+				status = AUTO_GREEN;
+				setTimer1(3000);
+			}
+			break;
+		case AUTO_GREEN:
+			HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_SET);
+			//TODO
+			if(timer1_flag == 1) {
+				status = AUTO_YELLOW;
+				setTimer1(2000);
+			}
+			break;
+		case AUTO_YELLOW:
+			//TODO
+			HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_SET);
+			if(timer1_flag == 1) {
+				status = AUTO_RED;
+				setTimer1(5000);
+			}
+			break;
+		default:
+			break;
 	}
 }
 
